2bafc2dd62a7a81d3a7c76a202724757e6ad13f4
Derek <dwl5986@rit.edu>
2016-02-23 11:09:02 -0500

    Added projects to repository.

---
+++ SyncingData_Part1/src/server.js
@@ -0,0 +1,40 @@
+var http = require('http');
+var fs = require('fs');
+var socketio = require('socket.io');
+var port = process.env.PORT || process.env.NODE_PORT || 3000;
+
+//read the client html file into memory
+//__dirname in node is the current directory
+//in this case the same folder as the server js file
+var index = fs.readFileSync(__dirname + '/../client/client.html');
+
+function onRequest(request, response) {
+
+ response.writeHead(200, {"Content-Type": "text/html"});
+ response.write(index);
+ response.end();
+}
+var app = http.createServer(onRequest).listen(port);
+console.log("Listening on 127.0.0.1:" + port);
+
+//pass in the http server into socketio and grab the websocket server as io
+var io = socketio(app);
+var serverValue = 0;
+
+// When there is a server connection
+io.sockets.on("connection", function(socket) {
+
+  socket.join('Main Room');
+
+  socket.on('updateScore', function(data) {
+    serverValue += data;
+    io.sockets.in('Main Room').emit('updatePara', serverValue);
+  });
+
+  //When someone disconnects, remove their websocket from the socket room
+  socket.on('disconnect', function(data) {
+    socket.leave('Main Room');
+  });
+});
+
+console.log('websocket server started');
